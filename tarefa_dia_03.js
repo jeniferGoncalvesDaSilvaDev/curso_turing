b886cec4cd2d7fc9cac8addb8e0a2e95a62713c7
Johann Bates <49965702+HanzoYagami@users.noreply.github.com>
2022-04-28 19:21:22 -0300

    segunda versão da tarefa para o curso Ituring

---
+++ tarefa_dia_03.js
@@ -0,0 +1,78 @@
+let a = 10
+let b = true
+let c = []
+let d = [1,2,3,4]
+let e = "Nome"
+ // 1 
+
+if (a ==10){
+    console.log("A")
+}else{
+    console.log("B")
+}// outuput = A
+
+//2
+if(a > 10){
+    console.log("A")
+}else{
+    console.log("B")
+}// Output = B
+
+// 3 
+
+if(a >=10){
+    console.log("A")
+}else{
+    console.log("B")
+}// Output = A
+
+
+// 4 
+
+if (b == true){
+    console.log("A")
+}else{
+    console.log("A")
+}// Output= A
+
+// 5 
+if (b){
+    console.log("A")
+}else{
+    console.log("B")
+}// Output= A
+
+// 6 
+if (c){
+    console.log("A")
+}else{
+    console.log("B")
+}// Output = A
+
+// 7 
+if(d){
+    console.log("A")
+}else{
+    console.log("B")
+}// Output = A
+
+// 8 
+if (d[0] == 1){
+    console.log("A")
+}else{
+    console.log("B")
+}// Output = A
+
+// 9 
+if (d[1] == 1){
+    console.log("A")
+}else{
+    console.log("B")
+}// Output =B
+
+// 10 
+if(typeof(e)== 'string'){
+    console.log("A")
+}else{
+    console.log("B")
+}// Output =A
